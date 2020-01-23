ad1dd5338ca5856caa668d681290fff051de2ee4
Debbie <debiurquhart@live.co.uk>
2020-01-23 16:34:57 +0000

    adds addional content

--- src/App.vue
+++ src/App.vue
@@ -11,11 +11,14 @@
       <div class="about">
         <h2>About Me</h2>
         <p>
-          Debbie enjoys programming, exercising, travelling, and food. She has recently decided to embark on a more thought provoking career path. Having completed
+          Debbie enjoys programming, exercising, travelling, and food.
+          She has recently decided to embark on a more thought provoking career path.
+          Having completed
           <a
             href="https://codeclan.com/courses/professional-software-development/"
             target="blank"
-          >CodeClan’s</a> immersive 16 week Professional Software Development course, she is looking forward to challenging herself daily with her newfound skills.
+          >CodeClan’s</a> immersive 16 week Professional Software Development course,
+          she is looking forward to challenging herself daily with her newfound skills.
         </p>
       </div>
       <div class="contact">
