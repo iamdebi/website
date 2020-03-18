9bff492b8d208c973a0e0d0235eb6dcd42285bb7
Debbie Urquhart <debiurquhart@live.co.uk>
2020-03-18 12:02:13 +0000

    adds social and project components

--- src/App.vue
+++ src/App.vue
@@ -27,71 +27,7 @@
         </div>
       </div>
       <div class="contact">
-        <h2>Contact</h2>
-        <a href="https://www.linkedin.com/in/debbie-urquhart/" target="blank">
-          <div class="linkedin social-container">
-            <p class="linkedin">LinkedIn</p>
-            <div class="svg-container">
-              <svg>
-                <path
-                  fill="#0077B5"
-                  d="M6.5 21.5h-5v-13h5v13zM4 6.5C2.5 6.5 1.5 5.3 1.5 4s1-2.4 2.5-2.4c1.6 0 2.5 1 2.6 2.5 0 1.4-1 2.5-2.6 2.5zm11.5 6c-1 0-2 1-2 2v7h-5v-13h5V10s1.6-1.5 4-1.5c3 0 5 2.2 5 6.3v6.7h-5v-7c0-1-1-2-2-2z"
-                />
-              </svg>
-            </div>
-          </div>
-        </a>
-        <a href="mailto:debiurquhart@live.co.uk" target="blank">
-          <div class="email social-container">
-            <p class="email">Email</p>
-            <div class="svg-container">
-              <svg>
-                <path
-                  fill="#4F4E4E"
-                  d="M12 0C5.38 0 0 5.38 0 12s5.38 12 12 12 12-5.38 12-12S18.62 0 12 0zm8 16c0 1.1-.9 2-2 2H6c-1.1 0-2-.9-2-2V8c0-1.1.9-2 2-2h12c1.1 0 2 .9 2 2v8z"
-                />
-                <path
-                  fill="#4F4E4E"
-                  d="M17.9 8.18c-.2-.2-.5-.24-.72-.07L12 12.38 6.82 8.1c-.22-.16-.53-.13-.7.08s-.15.53.06.7l3.62 2.97-3.57 2.23c-.23.14-.3.45-.15.7.1.14.25.22.42.22.1 0 .18-.02.27-.08l3.85-2.4 1.06.87c.1.04.2.1.32.1s.23-.06.32-.1l1.06-.9 3.86 2.4c.08.06.17.1.26.1.17 0 .33-.1.42-.25.15-.24.08-.55-.15-.7l-3.57-2.22 3.62-2.96c.2-.2.24-.5.07-.72z"
-                />
-              </svg>
-            </div>
-          </div>
-        </a>
-        <a href="https://twitter.com/DebbieCodes" target="blank">
-          <div class="twitter social-container">
-            <p class="twitter">Twitter</p>
-            <div class="svg-container">
-              <svg>
-                <path
-                  fill="#1DA1F2"
-                  d="M12 0C5.38 0 0 5.38 0 12s5.38 12 12 12 12-5.38 12-12S18.62 0 12 0zm5.26 9.38v.34c0 3.48-2.64 7.5-7.48 7.5-1.48 0-2.87-.44-4.03-1.2 1.37.17 2.77-.2 3.9-1.08-1.16-.02-2.13-.78-2.46-1.83.38.1.8.07 1.17-.03-1.2-.24-2.1-1.3-2.1-2.58v-.05c.35.2.75.32 1.18.33-.7-.47-1.17-1.28-1.17-2.2 0-.47.13-.92.36-1.3C7.94 8.85 9.88 9.9 12.06 10c-.04-.2-.06-.4-.06-.6 0-1.46 1.18-2.63 2.63-2.63.76 0 1.44.3 1.92.82.6-.12 1.95-.27 1.95-.27-.35.53-.72 1.66-1.24 2.04z"
-                />
-              </svg>
-            </div>
-          </div>
-        </a>
-        <a href="https://github.com/iamdebi" target="blank">
-          <div class="github social-container">
-            <p class="github">GitHub</p>
-            <div class="svg-container">
-              <svg
-                aria-hidden="true"
-                class="octicon octicon-mark-github"
-                height="24"
-                version="1.1"
-                viewBox="0 0 16 16"
-                width="24"
-              >
-                <path
-                  fill-rule="evenodd"
-                  fill="#4F4E4E"
-                  d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"
-                />
-              </svg>
-            </div>
-          </div>
-        </a>
+        <Social />
       </div>
     </div>
     <div class="projects">
@@ -99,6 +35,13 @@
         <div class="project-tile">
           <a href="http://debbie-bucket-list.herokuapp.com/" target="blank">
             <div class="ruby-tile"></div>
+            <p></p>
+          </a>
+        </div>
+
+        <div class="project-tile">
+          <a href="http://debbie-bucket-list.herokuapp.com/" target="blank">
+            <div class="js-tile"></div>
           </a>
         </div>
       </div>
@@ -107,8 +50,8 @@
 </template>
 
 <script>
-import FirstPerson from "./components/first.vue";
-import ThirdPerson from "./components/third.vue";
+import Social from "./components/social.vue";
+import Project from "./components/project.vue";
 
 export default {
   name: "app",
@@ -120,8 +63,8 @@ export default {
   },
 
   components: {
-    "first-person": FirstPerson,
-    "third-person": ThirdPerson
+    Social: Social,
+    Project: Project
   }
 };
 </script>
--- src/components/project.vue
+++ src/components/project.vue
@@ -6,7 +6,7 @@
 
 <script>
 export default {
-  name: "thirdPerson"
+  name: "Project"
 };
 </script>
 
