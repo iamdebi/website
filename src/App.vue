fc55348da1fa2ebe89eb27e8a9afc0c049f3883b
Debbie Urquhart <debiurquhart@live.co.uk>
2020-05-26 15:40:26 +0100

    styles load page on home screen

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,25 @@
 <template lang="html">
   <div id="app" class="main">
     <div class="header">
-      <h1>Debbie Urquhart</h1>
-      <div class="img-container">
-        <img class="border circle" src="../public/me.jpg" alt="Debbie Urquhart" />
+      <div class="header-container">
+        <div class="img-container">
+          <img class="border circle" src="../public/me.jpg" alt="Debbie Urquhart" />
+        </div>
+        <div class="bio">
+          <h1> Hello world, I'm <span class="my-name">Debbie Urquhart</Span>.</h1>
+          <h1>A Junior Full Stack Software Developer. </h1>
+        </div>
+        <div class="project-link">
+          <button class="btn" type="button" onClick="window.location.href='#project'">
+            <span class="circle">
+              <span class="icon arrow"></span>
+            </span>
+            <span id="project-btn" class="button-text">Checkout my work</span>
+          </button>
+        </div>
       </div>
-      <h2>Junior Software Developer</h2>
     </div>
+    <div class="body">
     <div class="about-contact">
       <div class="about">
         <div class="about-info">
@@ -22,11 +35,7 @@
             she is looking forward to challenging herself daily with her newfound skills.
           </p>
         </div>
-        <div class="project-link">
-          <a href="#project">
-            <h2>Projects</h2>
-          </a>
-        </div>
+        
       </div>
       <div class="contact">
         <Social />
@@ -35,6 +44,7 @@
     <div class="project">
       <Project-Container />
     </div>
+  </div>
   </div>
 </template>
 
@@ -59,119 +69,5 @@ export default {
 </script>
 
 <style lang="css">
-body {
-  margin: 0;
-  height: 100vh;
-  font-family: "Josefin Sans", sans-serif;
-}
-
-h1,
-h2 {
-  color: #4f4e4e;
-}
-
-a {
-  text-decoration: none;
-  color: #00a2cd;
-}
-
-.header {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  background-color: #c8c8c8;
-}
-
-.border {
-  border-style: solid;
-  border-width: 1px;
-  border-color: rgba(0, 0, 0, 0.125);
-}
-
-.circle {
-  border-radius: 50%;
-}
-
-.about-contact {
-  display: flex;
-  justify-content: center;
-  padding: 0 0 60px 0;
-}
-
-@media (max-width: 767px) {
-  .about-contact {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-  }
-
-  .contact {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
-
-  .about {
-    width: 50vw;
-    text-align: center;
-    margin: 0 5% 0 25%;
-    max-width: 500px;
-    padding-left: 15%;
-  }
-
-  .project {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-  }
-}
-
-.project-link {
-  border: solid 1px #c8c8c8;
-  margin: 10%;
-}
-
-.about {
-  width: 50vw;
-  text-align: center;
-  max-width: 500px;
-  margin-left: 10%;
-}
-
-.contact {
-  margin-left: 5%;
-}
-
-.social-container {
-  display: flex;
-  align-items: center;
-  flex-direction: row-reverse;
-  justify-content: flex-end;
-}
-
-.linkedin {
-  color: #0077b5;
-}
-
-.twitter {
-  color: #1da1f2;
-}
-
-.email,
-.github {
-  color: #4f4e4e;
-}
-
-svg {
-  width: 100%;
-  height: 100%;
-  overflow: unset;
-}
-
-.svg-container {
-  position: relative;
-  height: 24px;
-  width: 24px;
-  margin: 0 10px 0 0;
-}
+ @import './styles/app.css';
 </style>
